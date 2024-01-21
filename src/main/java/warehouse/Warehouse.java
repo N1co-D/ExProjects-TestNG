809378d4cccee6c29d9b78c3107aaf92ea0cc9b5
N1co-D <122123347+N1co-D@users.noreply.github.com>
2024-01-21 11:32:57 +0300

    Add files via upload

---
+++ src/main/java/warehouse/Warehouse.java
@@ -0,0 +1,34 @@
+package warehouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Warehouse {
+    private List<Product> products;
+
+    public Warehouse() {
+        this.products = new ArrayList<>();
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public Product findProductById(int productId) {
+        for (Product product : products) {
+            if (product.getId() == productId) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
+
